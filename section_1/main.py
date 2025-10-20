f0b5dd71b41917cc87ee2be3f4e681c6df28b3ec
Adle24 <askaradilet41@gmail.com>
2025-10-20 19:55:16 +0500

    finished first section of bootcamp

---
+++ section_1/main.py
@@ -0,0 +1,34 @@
+from langchain.agents import create_agent
+from langchain.agents.structured_output import ToolStrategy
+from langchain_ollama import ChatOllama
+from pydantic import BaseModel
+
+
+class Answer(BaseModel):
+    answer: str
+    justification: str
+
+
+model = ChatOllama(model="qwen3:4b", temperature=0)
+agent = create_agent(
+    model,
+    system_prompt="You are a helpful assistant that responds to questions with three exclamation marks.",
+    response_format=ToolStrategy(Answer)
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What weighs more, a pound of bricks or a pound of feathers?"}]}
+)
+
+# for chunk in agent.stream({
+#     "messages": [
+#         {
+#             "role": "user",
+#             "content": "Search for AI news and summarize the findings"
+#         }
+#     ]
+# }, stream_mode="values"):
+#     latest_message = chunk["messages"][-1]
+#
+#     if latest_message.content:
+#         print(f"agent: {latest_message.content}")
